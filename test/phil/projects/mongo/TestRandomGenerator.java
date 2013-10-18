6d84a892a4e00412a543bd6a2bd93308cc0da974
Phil <signup@lightweaver.info>
2013-10-18 12:52:34 +0100

    Removed unused imports

--- test/phil/projects/mongo/TestRandomGenerator.java
+++ test/phil/projects/mongo/TestRandomGenerator.java
@@ -7,8 +7,6 @@ import static org.junit.Assert.*;
 
 import java.util.HashSet;
 
-import org.junit.After;
-import org.junit.Before;
 import org.junit.Test;
 
 import phil.projects.mongo.RandomGenerator.UniqueStringSaturationException;
